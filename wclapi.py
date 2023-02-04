b1bf181abc51955d727915f75b2279e85387d5d3
Dennis Israelsson <github@mdh.nu>
2023-02-04 15:03:33 +0100

    add /deaths command

--- wclapi.py
+++ wclapi.py
@@ -2,7 +2,7 @@
 """Attendance class definition."""
 from collections import defaultdict
 from datetime import datetime, timezone
-from typing import Any
+from gql.transport.exceptions import TransportServerError
 from gql.transport.requests import RequestsHTTPTransport
 from gql import Client, gql
 from tokencache import gettoken
@@ -11,16 +11,108 @@ from tokencache import gettoken
 class WCL:
     """Retrieve data from WCL API."""
 
-    def __init__(self) -> None:
+    def __init__(self, zone: int, start_date: str, end_date: str, tag: int, encounter: int) -> None:
         """Initiate class."""
         url = "https://classic.warcraftlogs.com/api/v2/client"
         token = gettoken()
         transport = RequestsHTTPTransport(url=url, headers={"Authorization": token})
         self.client = Client(transport=transport, fetch_schema_from_transport=True)
+        self.zone = zone
+        self.start_date = start_date
+        self.end_date = end_date
+        self.tag = tag
+        self.encounter = encounter
+        self.players: dict[str, int] = defaultdict(int)
 
-    def _zone_attendance(self, zone: int) -> list[dict[str, Any]]:
-        """Retrieve attendance data for WCL zone."""
-        query = gql(
+    def _get_all_results(self, query: str, querytype: str, params: dict[str, int]) -> list:
+        """Get all results from query."""
+        gqlquery = gql(query)
+        data = []
+        page = 1
+
+        while page is not False:
+            params["page"] = page
+            try:
+                result = self.client.execute(gqlquery, variable_values=params)
+            except TransportServerError as err:
+                raise ConnectionError(err) from err
+
+            if querytype == "reportData":
+                data += result["reportData"]["reports"]["data"]  # pylint: disable=unsubscriptable-object
+                if result["reportData"]["reports"]["has_more_pages"]:  # pylint: disable=unsubscriptable-object
+                    page += 1
+                else:
+                    page = False
+            elif querytype == "guildData":
+                data += result["guildData"]["guild"]["attendance"]["data"]  # pylint: disable=unsubscriptable-object
+                if result["guildData"]["guild"]["attendance"]["has_more_pages"]:  # pylint: disable=unsubscriptable-object
+                    page += 1
+                else:
+                    page = False
+
+        return data
+
+    def _convert_dates(self) -> tuple[int, int]:
+        """Convert dates from string to utime."""
+        if self.start_date:
+            start_datetime = datetime.fromisoformat(self.start_date)
+            start_utime = int(start_datetime.replace(tzinfo=timezone.utc).timestamp()) * 1000
+        else:
+            start_utime = 0
+        if self.end_date:
+            end_datetime = datetime.fromisoformat(self.end_date)
+            end_utime = int(end_datetime.replace(tzinfo=timezone.utc).timestamp()) * 1000 + 86400000
+        else:
+            end_utime = 9999999999999
+
+        return start_utime, end_utime
+
+    def _encounter_reports(self) -> list[str]:
+        """Retrieve raid reports for specific encounter."""
+        query = (
+            """
+            query ($page: Int, $zone: Int, $encounter: Int) {
+                reportData {
+                    reports(guildID: 611338, guildTagID: 50758, limit: 100,
+                            page: $page, zoneID: $zone) {
+                        total
+                        has_more_pages
+                        data {
+                            code
+                            fights(encounterID: $encounter) {
+                                id
+                            }
+                        }
+                    }
+                }
+            }
+        """
+        )
+
+        params = {"zone": self.zone, "encounter": self.encounter}
+        data = self._get_all_results(query, "reportData", params)
+        encounter_reports: list[str] = []
+
+        for report in data:
+            if report["fights"]:
+                encounter_reports.append(report["code"])
+
+        return encounter_reports
+
+    def _combine_alts(self) -> None:
+        """Combine alts attendance to main."""
+        if {"Liltkw", "Tkwxd"} <= self.players.keys():
+            self.players["Liltkw"] += self.players["Tkwxd"]
+            self.players.pop("Tkwxd")
+
+    def calculate_attendance(self) -> tuple[dict[str, int], int]:
+        """Calculate attendance according to args."""
+        start_utime, end_utime = self._convert_dates()
+
+        if self.encounter:
+            encounter_reports = self._encounter_reports()
+
+        query = (
             """
             query ($page: Int, $zone: Int) {
                 guildData {
@@ -44,32 +136,64 @@ class WCL:
             }
         """
         )
-        data = []
-        page = 1
-        while page is not False:
-            params = {"page": page, "zone": zone}
-            result = self.client.execute(query, variable_values=params)
-            data += result["guildData"]["guild"]["attendance"]["data"]  # pylint: disable=unsubscriptable-object
-            if result["guildData"]["guild"]["attendance"]["has_more_pages"] is True:  # pylint: disable=unsubscriptable-object
-                page += 1
-            else:
-                page = False
-        return data
+        params = {"zone": self.zone}
+        data = self._get_all_results(query, "guildData", params)
+        total_raids = 0
 
-    def _raid_reports(self, zone: int, encounter: int) -> list[str]:
+        for report in data:
+            if self.start_date and start_utime > report["startTime"]:
+                continue
+            if self.end_date and end_utime < report["startTime"]:
+                continue
+            if self.encounter and report["code"] not in encounter_reports:
+                continue
+
+            for player in report["players"]:
+                self.players[player["name"]] += 1
+            total_raids += 1
+
+        self._combine_alts()
+        return self.players, total_raids
+
+    @staticmethod
+    def _avg_deaths(death_stats: dict[str, dict]) -> dict[str, float]:
+        """Return average deaths per player dict."""
+        player_stats: dict[str, float] = {}
+        for player, player_stat in death_stats.items():
+            if player_stat["reports"] < 5:
+                continue
+            try:
+                avg = float(player_stat['deaths'] / player_stat['reports'])
+            except ZeroDivisionError:
+                avg = float(0)
+            player_stats[player] = avg
+
+        return player_stats
+
+    def calculate_deaths(self) -> dict[str, float]:
         """Retrieve raid reports for specific encounter."""
-        query = gql(
+        start, end = self._convert_dates()
+
+        if self.encounter:
+            encounter_reports = self._encounter_reports()
+
+        query = (
             """
-            query ($page: Int, $zone: Int, $encounter: Int) {
+            query ($end: Float, $tag: Int, $page: Int, $start: Float, $zone: Int) {
                 reportData {
-                    reports(guildID: 611338, guildTagID: 50758, limit: 100, page: $page, zoneID: $zone) {
+                    reports(endTime: $end, guildID: 611338, guildTagID: $tag, limit: 100,
+                            page: $page, startTime: $start, zoneID: $zone) {
                         total
                         has_more_pages
                         data {
                             code
-                            fights(encounterID: $encounter) {
+                            fights(killType: Wipes) {
                                 id
                             }
+                            rankedCharacters {
+                                name
+                            }
+                            table(dataType: Deaths, endTime: $end)
                         }
                     }
                 }
@@ -77,49 +201,29 @@ class WCL:
         """
         )
 
-        encounter_reports: list[str] = []
-        page = 1
-        while page is not False:
-            params = {"page": page, "zone": zone, "encounter": encounter}
-            result = self.client.execute(query, variable_values=params)
-            for report in result["reportData"]["reports"]["data"]:  # pylint: disable=unsubscriptable-object
-                if report["fights"]:
-                    encounter_reports.append(report["code"])
-            if result["reportData"]["reports"]["has_more_pages"] is True:  # pylint: disable=unsubscriptable-object
-                page += 1
-            else:
-                page = False
+        params = {"end": end, "tag": self.tag, "start": start, "zone": self.zone}
+        data = self._get_all_results(query, "reportData", params)
 
-        return encounter_reports
-
-    def calculated_attendance(self,
-                              zone: int,
-                              start_date: str = "",
-                              end_date: str = "",
-                              encounter: int = 0) -> tuple[dict[str, int], int]:
-        """Calculate attendance according to args."""
-        if start_date:
-            start_datetime = datetime.fromisoformat(start_date)
-            start_utime = int(start_datetime.replace(tzinfo=timezone.utc).timestamp()) * 1000
-        if end_date:
-            end_datetime = datetime.fromisoformat(end_date)
-            end_utime = int(end_datetime.replace(tzinfo=timezone.utc).timestamp()) * 1000 + 86400000
-
-        players_attendance: dict[str, int] = defaultdict(int)
-        total_raids = 0
-        attendance_data = self._zone_attendance(zone)
-        if encounter:
-            encounter_reports = self._raid_reports(zone, encounter)
-
-        for report in attendance_data:
-            if start_date and start_utime > report["startTime"]:
+        death_stats: dict[str, dict] = {}
+        for report in data:
+            if self.encounter and report["code"] not in encounter_reports:
                 continue
-            if end_date and end_utime < report["startTime"]:
-                continue
-            if encounter and report["code"] not in encounter_reports:
-                continue
-            for player in report["players"]:
-                players_attendance[player["name"]] += 1
-            total_raids += 1
+            wipes: list[int] = []
+            for fight in report["fights"]:
+                wipes.append(fight["id"])
+            for player in report["rankedCharacters"]:
+                if player["name"] not in death_stats:
+                    death_stats[player["name"]] = {"reports": 0, "deaths": 0}
+                death_stats[player["name"]]["reports"] += 1
 
-        return players_attendance, total_raids
+            for entry in report["table"]["data"]["entries"]:
+                # Ignore priest double deaths due to spirit form
+                if all([entry["icon"] == "Priest-Holy",
+                        entry["damage"]["abilities"],
+                        entry["events"]]):
+                    continue
+
+                if entry["fight"] not in wipes:
+                    death_stats[entry["name"]]["deaths"] += 1
+
+        return self._avg_deaths(death_stats)
--- wclbot.py
+++ wclbot.py
@@ -8,6 +8,72 @@ from settings import BOT_TOKEN
 bot = Snake(intents=Intents.DEFAULT)
 
 
+def zone_option():  # type: ignore
+    """Start zone option."""
+
+    def wrapper(func):  # type: ignore
+        return slash_option(
+            name="zone",
+            description="Choose a zone",
+            required=True,
+            opt_type=OptionTypes.INTEGER,
+            choices=[
+                SlashCommandChoice(name="Naxx/Sarth/Maly", value=1015),  # type: ignore
+                SlashCommandChoice(name="VoA", value=1016),  # type: ignore
+                SlashCommandChoice(name="Ulduar", value=1017),  # type: ignore
+            ]
+        )(func)
+
+    return wrapper
+
+
+def start_date_option():  # type: ignore
+    """Start date option."""
+
+    def wrapper(func):  # type: ignore
+        return slash_option(
+            name="start_date",
+            description="ISO8601 date | Example: 2022-04-01",
+            required=False,
+            opt_type=OptionTypes.STRING
+        )(func)
+
+    return wrapper
+
+
+def end_date_option():  # type: ignore
+    """End date option."""
+
+    def wrapper(func):  # type: ignore
+        return slash_option(
+            name="end_date",
+            description="ISO8601 date | Example: 2022-04-31",
+            required=False,
+            opt_type=OptionTypes.STRING
+        )(func)
+
+    return wrapper
+
+
+def encounter_option():  # type: ignore
+    """End date option."""
+
+    def wrapper(func):  # type: ignore
+        return slash_option(
+            name="encounter",
+            description="Choose a specific raid (Naxx/Sarth/Maly only)",
+            required=False,
+            opt_type=OptionTypes.INTEGER,
+            choices=[
+                SlashCommandChoice(name="Naxx", value=101119),  # type: ignore
+                SlashCommandChoice(name="Sarth", value=742),  # type: ignore
+                SlashCommandChoice(name="Maly", value=734),  # type: ignore
+            ]
+        )(func)
+
+    return wrapper
+
+
 @listen()
 async def on_ready() -> None:
     """Event called when the bot is ready to respond to commands."""
@@ -16,66 +82,125 @@ async def on_ready() -> None:
 
 
 @slash_command(name="attendance", description="Show the attendance statistics based on logs")
-@slash_option(
-    name="zone",
-    description="Choose a zone",
-    required=True,
-    opt_type=OptionTypes.INTEGER,
-    choices=[
-        SlashCommandChoice(name="BT/MH", value=1011),  # type: ignore
-        SlashCommandChoice(name="SSC/TK", value=1010),  # type: ignore
-    ]
-)
-@slash_option(
-    name="start_date",
-    description="ISO8601 date | Example: 2022-04-01",
-    required=False,
-    opt_type=OptionTypes.STRING
-)
-@slash_option(
-    name="end_date",
-    description="ISO8601 date | Example: 2022-04-31",
-    required=False,
-    opt_type=OptionTypes.STRING
-)
-@slash_option(
-    name="encounter",
-    description="Choose a specific raid",
-    required=False,
-    opt_type=OptionTypes.INTEGER,
-    choices=[
-        SlashCommandChoice(name="BT", value=601),  # type: ignore
-        SlashCommandChoice(name="MH", value=618)  # type: ignore
-    ]
-)
-async def attendance_function(ctx: InteractionContext, zone: int, start_date: str = "",
-                              end_date: str = "", encounter: int = 0) -> None:
+@zone_option()
+@start_date_option()
+@end_date_option()
+@encounter_option()
+async def attendance_function(ctx: InteractionContext,
+                              zone: int,
+                              start_date: str = "",
+                              end_date: str = "",
+                              encounter: int = 0) -> None:
     """Discord embed response with attendance data."""
     await ctx.defer()
-    wcl = WCL()
-    players_attendance, total_raids = wcl.calculated_attendance(zone=zone,
-                                                                start_date=start_date,
-                                                                end_date=end_date,
-                                                                encounter=encounter)
+    wcl = WCL(zone=zone,
+              start_date=start_date,
+              end_date=end_date,
+              tag=50758,
+              encounter=encounter)
+    try:
+        players_attendance, total_raids = wcl.calculate_attendance()
+    except ConnectionError as err:
+        await ctx.send(f"Something went wrong. Error message {err}")
+        raise Exception from err
     output = Embed()
     output.title = "Attendance"
     text = []
+
+    if encounter:
+        if encounter == 101119:
+            field_name = "Naxx"
+        elif encounter == 742:
+            field_name = "Sarth"
+        elif encounter == 734:
+            field_name = "Maly"
+    else:
+        if zone == 1015:
+            field_name = "Naxx/Sarth/Maly"
+        elif zone == 1016:
+            field_name = "VoA"
+        elif zone == 1017:
+            field_name = "Ulduar"
+    if start_date or end_date:
+        field_name += f" ({start_date}-{end_date})"
+
     for player, value in sorted(players_attendance.items(), key=lambda item: item[1], reverse=True):
         percentage = value / total_raids * 100
+        percentage = min(percentage, 100)
         text.append(f"{player}: {percentage:.0f}%")
-    if encounter:
-        if encounter == 601:
-            field_name = "BT"
-        elif encounter == 618:
-            field_name = "MH"
-    else:
-        if zone == 1011:
-            field_name = "BT/MH"
-        elif zone == 1010:
-            field_name = "SSC/TK"
-    field_name += f" ({start_date}-{end_date})"
 
-    output.add_field(name=field_name, value="\n".join(text))
+    for i in range(0, len(text), 15):
+        if i != 0:
+            field_name = "\u200b"
+        output.add_field(name=field_name, inline=True, value="\n".join(text[i:i + 15]))
+
+    await ctx.send(embed=output)
+
+
+@slash_command(name="deaths", description="Show the deaths statistics based on logs")
+@zone_option()
+@slash_option(
+    name="tag",
+    description="Choose a specific tag",
+    required=True,
+    opt_type=OptionTypes.INTEGER,
+    choices=[
+        SlashCommandChoice(name="Main raid", value=50758),  # type: ignore
+        SlashCommandChoice(name="Pug", value=50705)  # type: ignore
+    ]
+)
+@start_date_option()
+@end_date_option()
+@encounter_option()
+async def deaths_function(ctx: InteractionContext,
+                          zone: int,
+                          tag: int,
+                          start_date: str = "",
+                          end_date: str = "",
+                          encounter: int = 0) -> None:
+    """Discord embed response with attendance data."""
+    await ctx.defer()
+
+    wcl = WCL(zone=zone, start_date=start_date, end_date=end_date,
+              tag=tag, encounter=encounter)
+    try:
+        player_stats = wcl.calculate_deaths()
+    except ConnectionError as err:
+        await ctx.send(f"Something went wrong. Error message {err}")
+        raise Exception(err) from err
+
+    if encounter:
+        if encounter == 101119:
+            field_name = "Naxx"
+        elif encounter == 742:
+            field_name = "Sarth"
+        elif encounter == 734:
+            field_name = "Maly"
+    else:
+        if zone == 1015:
+            field_name = "Naxx/Sarth/Maly"
+        elif zone == 1016:
+            field_name = "VoA"
+        elif zone == 1017:
+            field_name = "Ulduar"
+        else:
+            field_name = ""
+
+    if tag == 50705:
+        field_name += " pugs"
+    if start_date or end_date:
+        field_name += f" ({start_date}-{end_date})"
+
+    output = Embed()
+    output.title = "Average deaths per raid (minimum five raids and wipes excluded)"
+    text = []
+    for player, value in sorted(player_stats.items(), key=lambda item: item[1], reverse=True):
+        text.append(f"{player}: {value:.1f}")
+
+    for i in range(0, len(text), 15):
+        if i != 0:
+            field_name = "\u200b"
+        output.add_field(name=field_name, inline=True, value="\n".join(text[i:i + 15]))
     await ctx.send(embed=output)
 
 
